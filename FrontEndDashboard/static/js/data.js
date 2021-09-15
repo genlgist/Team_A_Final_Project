5de231b7781bbd6115ff9a1718485a97002a0495
Christopheremorgan <75141661+Christopheremorgan@users.noreply.github.com>
2021-09-14 20:55:43 -0400

    Initial dashboard mockup

---
+++ FrontEndDashboard/static/js/data.js
@@ -0,0 +1,17 @@
+var data = [
+  {
+    expression: "happy",
+    predictscore: "56%",
+    emoji: "link to image to read in?",
+  },
+  {
+    expression: "surprise",
+    predictscore: "23%",
+    emoji: "link to image to read in?",
+  },
+  {
+    expression: "neutral",
+    predictscore: "22%",
+    emoji: "link to image to read in?",
+  }
+];
