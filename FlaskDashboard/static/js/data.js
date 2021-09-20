e47d3eb01aaa6237e1350b942d21a8dbf87c360f
Christopheremorgan <75141661+Christopheremorgan@users.noreply.github.com>
2021-09-20 19:28:37 -0400

    flask and js integration

---
+++ FlaskDashboard/static/js/data.js
@@ -0,0 +1,14 @@
+var data = [
+  {
+    expression: "happy",
+    predictscore: "56%",
+  },
+  {
+    expression: "surprise",
+    predictscore: "23%",
+  },
+  {
+    expression: "neutral",
+    predictscore: "22%",
+  }
+];
