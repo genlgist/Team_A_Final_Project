6188a9ee66d65c0b4ff8f45de63e251567addbe2
morall20 <82338072+morall20@users.noreply.github.com>
2021-09-24 22:00:29 -0400

    last upload
    last upload for the night

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import os
 import tensorflow as tf
+import urllib.request
+from werkzeug.utils import secure_filename
 from tensorflow import keras
 from tensorflow.keras.models import load_model
 import PIL
@@ -14,63 +16,75 @@ import warnings
 from flask import Flask, render_template, redirect, request, url_for
 from flask import jsonify
 
+##### STORAGE LOCATION #####
+UPLOAD_FOLDER = 'static/images/'
+
 ####### SETTING UP FLASK #########
 # defining flask app
-app = Flask(__name__) 
+app = Flask(__name__)
+app.run(DEBUG=True)
 
 ## Flask configuration SECRET_KEY variable is needed for passing files into a POST request generates a new session. 
-app.config['SECRET_KEY'] = "supertopsecretprivatekey"
+app.config['SECRET_KEY'] = "ABC123#RDX$678qwv3"
+
+# Image Storage
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 48 * 48
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # load the saved model
 model = load_model("models/final_model.h5")  # put in resource folder with selected image
 print('Tensorflow keras Model loaded successfully')
 
-# Set Debugger
-app.run(debug = True)
-
 ######## CREATING ROUTES ########
 # welcome/index route
-@app.route("/", methods=['GET', 'POST'])
-# def index():
-#    print("index is running!")
-#    return render_template('index.html')
-   
-def home():
-    if request.method == 'GET':
-        # show the upload form
-        return render_template('index.html')
+@app.route("/")
+def index():
+    print("index is running!")
+    return render_template('index.html')
 
-    if request.method == 'POST':
-        # check if a file was passed into the POST request
-        if 'image' not in request.files:
-            print('No file was uploaded.')
-            return redirect(request.url)
-
-        image_file = request.files['image']
-
-        # if filename is empty, then assume no upload
-        if image_file.filename == '':
-            print('No file was uploaded.')
-            return redirect(request.url)
-
-        # if the file is "legit"
-        if image_file:
-            passed = False
-            try:
-                filename = image_file.filename
-                filepath = os.path.join('/static/images', filename)
-                image_file.save(filepath)
-                passed = True
-            except Exception:
-                passed = False
-
-            if passed:
-                return redirect(url_for('predict', filename=filename))
-            else:
-                print('An error occurred, try again.')
-                return redirect(request.url)
+@app.route("/", methods =['POST'])   
+def upload_image():
+    if 'file' not in request.files:
+        print('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    print(request)
+    if file.filename == '':
+        print('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        print('Image successfully uploaded and displayed below')
+        return render_template('index.html', filename= filename)
+    else:
+        print('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
 print("index is running!")
 
+@app.route('/display/<filename>')
+def display_image(filename):
+    print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='images/' + filename), code=301)
+
+@app.route('/read_file', methods=['GET'])
+def read_uploaded_file():
+    filename = secure_filename(request.args.get('filename'))
+    try:
+        if filename and allowed_file(filename):
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], filename)) as f:
+                print("successfully read image")
+                return f.read()
+    except IOError:
+        pass
+    return "Unable to read file"
 
 # creating route that will run the model and create data for data.js
 @app.route("/predict", methods=['GET'])
@@ -83,7 +97,7 @@ def predict():
     ######## IMPORTING IMAGE #########
 
     #import image
-    img = Image.open('#imageResult')  #point to js resource folder holding this file. hold trained model in the resoure folder as well
+    img = Image.open()  #point to js resource folder holding this file. hold trained model in the resoure folder as well
 
 
     # load the saved model
@@ -173,8 +187,10 @@ def predict():
 
     # having route return JSON
     return(data)
-
    
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('error.html'), 500    
+
+
     
-
-
